8c31adc9f102d8189fa9b7913c1dcbd4bfaafb01
ShushrushaSharma <shushrusha.sharma14@gmail.com>
2023-11-10 13:11:20 +0545

    Done Registration

--- BookingEngineApp/models.py
+++ BookingEngineApp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class UserRegistration(AbstractUser):
+    username = models.CharField(max_length=100,unique=True)
+    email = models.EmailField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+def __str__(self):
+        return self.username
